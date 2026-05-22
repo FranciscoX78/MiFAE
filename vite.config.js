0e996f81783ee0ec824fcd56859a62a42a441c2b
Francisco NeuroPlatform <franc@neuroplatform.ai>
2026-05-22 12:13:48 -0300

    Corrige base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-const basePath = process.env.NODE_ENV === 'production' ? '/mifae/' : '/';
+const basePath = process.env.NODE_ENV === 'production' ? '/MiFAE/' : '/';
 
 export default defineConfig({
   base: basePath,
