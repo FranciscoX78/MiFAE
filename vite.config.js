d4787dbc6f054487c56e87f9634028e4535c7107
Francisco NeuroPlatform <franc@neuroplatform.ai>
2026-05-26 21:29:35 -0300

    Disable PWA service worker minify

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
+      minify: false,
       includeAssets: ['icons/*.png'],
       manifest: {
         name: 'MiFAE',
